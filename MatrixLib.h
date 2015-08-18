f2f786e9c3757f60e2fdbac7cf6948af4725ab58
hellla <good7nick@gmail.com>
2015-08-18 17:01:10 +0300

    =Privet

--- MatrixLib.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-double **initMatrix(int, int);
-void printMatrix(double **, int, int);
-double **multiplyMatrices(double **, double **, int, int, int, int, int&, int&);
-double **getMatrFromFile(const char *, int&, int&);
